34faa3931608b5406c0818e08d2e6453de86a176
iekei <144453060+iekei@users.noreply.github.com>
2026-08-18 13:09:47 +0900

    Update main.js

--- game/world/main.js
+++ game/world/main.js
@@ -8,7 +8,7 @@ const map = L.map('map', {
 });
 
 // CartoDB Dark Matter（安定して高速読み込みされる暗色系世界地図タイル）
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://{s}[.basemaps.cartocdn.com/rastertiles/voyager/], {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   subdomains: 'abcd',
   maxZoom: 19
